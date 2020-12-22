b78c0cce96040d1acf863b051666960243ec655e
Maodoun <72317252+Maodoun@users.noreply.github.com>
2020-12-22 11:37:49 +0300

    Create Odev1.py

---
+++ Odev1.py
@@ -0,0 +1,17 @@
+#Turkish Ai Hub- Introduction to Python Course İlk Ödev
+
+name = input("Lutfen Adinizi giriniz: ")
+soyadi = input("Lutfen Soyadinizi giriniz: ")
+yasiniz = input("Lutfen Yaşinizi giriniz: ")
+sehir = input("Yaşadiginiz Şehiri giriniz: ")
+meslek = input("Lutfen Mesleginizi giriniz: ")
+
+print(f"Adınız: {name} ve Soyadınız: {soyadi}, Yaşınız: {yasiniz}, Bulunduğunuz Şehir: {sehir} ve Mesleğiniz: {meslek} ") 
+
+name = input("Lutfen Adinizi giriniz: ")
+soyadi = input("Lutfen Soyadinizi giriniz: ")
+yasiniz = input("Lutfen Yaşinizi giriniz: ")
+sehir = input("Yaşadiginiz Şehiri giriniz: ")
+meslek = input("Lutfen Mesleginizi giriniz: ")
+
+print("Adiniz: {} ve Soyadiniz: {}, Yasiniz: {}, Yasadiginiz Sehir: {} ve Mesleginiz: {} ".format(name ,soyadi, yasiniz, sehir, meslek ))
