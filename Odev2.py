76c50cc152d167f1580a5726076bd661901477f9
Maodoun <72317252+Maodoun@users.noreply.github.com>
2020-12-23 11:38:29 +0300

    Update Odev2.py

--- Odev2.py
+++ Odev2.py
@@ -1,12 +1,17 @@
 #Turkish Ai Hub- Introduction to Python Course İkinci Ödev
 
-name = input("Lutfen Adinizi giriniz: ")
-lastname = input("Lutfen Soyadinizi giriniz: ")
+name = str(input("Lutfen Adinizi giriniz: "))
+lastname = str(input("Lutfen Soyadinizi giriniz: "))
 age = int(input("Lutfen Yaşinizi giriniz: "))
 dateofbirth = int(input("Lütfen dogum yilinizi yaziniz: "))
 
 list1 = []
-list1.append(f"Adın: {name}, Soyadın: {lastname}, Yasın: {age}, Doğum yılın: {dateofbirth}")
+list1.append(name)
+list1.append(lastname)
+list1.append(age)
+list1.append(dateofbirth)
+
+print(list1)
 
 for i in list1:
     print(i)
@@ -15,3 +20,4 @@ if (age < 18):
     print("You can't go out because it's too dangerous")
 else:
     print("You can go out to the street")
+
