4898bb7c1aa52634213685518b75f8cca060b5c5
Maodoun <72317252+Maodoun@users.noreply.github.com>
2020-12-23 14:42:46 +0300

    Update Odev2.py

--- Odev2.py
+++ Odev2.py
@@ -1,15 +1,14 @@
 #Turkish Ai Hub- Introduction to Python Course İkinci Ödev
+list1 = []
 
 name = str(input("Lutfen Adinizi giriniz: "))
 lastname = str(input("Lutfen Soyadinizi giriniz: "))
 age = int(input("Lutfen Yaşinizi giriniz: "))
 dateofbirth = int(input("Lütfen dogum yilinizi yaziniz: "))
+list1.append([name, lastname, age, dateofbirth])
 
-list1 = []
-list1.append(name)
-list1.append(lastname)
-list1.append(age)
-list1.append(dateofbirth)
+#if type(name) != str:    KONTROL İÇİN AMA EKSİK BİR NOKTADA
+#    Print("Lütfen adinizi giriniz")
 
 for i in list1:
     print(i)
@@ -18,4 +17,3 @@ if (age < 18):
     print("You can't go out because it's too dangerous")
 else:
     print("You can go out to the street")
-
